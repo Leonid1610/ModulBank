402e279af87ff8bef8fc38ea73fb59b8dfe0f96a
Leonid <sandal.leonid@mail.ru>
2020-01-19 18:24:05 +0300

    изменен create_bodies

--- help_functions/create_bodies.py
+++ help_functions/create_bodies.py
@@ -22,16 +22,16 @@ body_with_left_negative_operand_for_division = Bodies(left_negative_operand, "/"
 # input_data для тестов на вычитание
 body_with_positive_numbers_for_difference = Bodies(left_positive_operand, "-", right_positive_operand)\
     .__dict__['body']
-body_with_negative_numbers_for_difference = Bodies(left_negative_operand / 2, "-", right_negative_operand/2)\
+body_with_negative_numbers_for_difference = Bodies(left_negative_operand / 2, "-", right_negative_operand / 2)\
     .__dict__['body']
-body_with_right_negative_operand_for_difference = Bodies(left_positive_operand, "-", right_negative_operand / 2)\
+body_with_right_negative_operand_for_difference = Bodies(left_positive_operand / 2, "-", right_negative_operand / 2)\
     .__dict__['body']
-body_with_left_negative_operand_for_difference = Bodies(left_negative_operand, "-", right_positive_operand / 2)\
+body_with_left_negative_operand_for_difference = Bodies(left_negative_operand / 2, "-", right_positive_operand / 2)\
     .__dict__['body']
 
 
 # input_data для тестов на сложение
-body_with_positive_numbers_for_sum = Bodies(left_positive_operand_for_sum, "+", right_positive_operand_for_sum)\
+body_with_positive_numbers_for_sum = Bodies(left_positive_operand, "+", right_positive_operand_for_sum)\
     .__dict__['body']
 body_with_negative_numbers_for_sum = Bodies(left_negative_operand_for_sum, "+", right_negative_operand_for_sum)\
     .__dict__['body']
