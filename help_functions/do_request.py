f2ec78fdd1e18adec8cc642ecc7eacad36367205
Leonid <sandal.leonid@mail.ru>
2020-01-19 20:44:28 +0300

    123

--- help_functions/do_request.py
+++ help_functions/do_request.py
@@ -5,6 +5,10 @@ from http import HTTPStatus
 
 
 def do_request_and_get_response_for_positive_tests(input_data):
+    """
+    :param input_data: request body
+    :return: response body
+    """
     req = post(url=Endpoints.url_calculate, headers=Endpoints.headers, data=json.dumps(input_data))
     assert req.status_code == HTTPStatus.OK.value
     result = req.json()['result']
@@ -12,6 +16,11 @@ def do_request_and_get_response_for_positive_tests(input_data):
 
 
 def do_request_and_get_response_for_negative_tests(input_data, expected_result):
+    """
+    :param input_data: request body
+    :param expected_result: ожидаемый статус код от сервера для теста
+    :return: response body
+    """
     req = post(url=Endpoints.url_calculate, headers=Endpoints.headers, data=json.dumps(input_data))
     assert req.status_code == expected_result, f"""Некорректный ответ от сервера: ожидается {expected_result}, получено 
                                                    {req.status_code}"""
