e3dfb88c697d09d6da8bdb46f799c072fb228aee
Leonid <sandal.leonid@mail.ru>
2020-01-19 20:54:01 +0300

    123

--- help_functions/generate_random_operation.py
+++ help_functions/generate_random_operation.py
@@ -4,9 +4,15 @@ from string import ascii_uppercase
 
 class SymbolGenerator(object):
     def __init__(self, count):
+        """
+        :param count: кол-во знаков, в случайно сгенерированной строке
+        """
         self.count = count
 
     def generate_random_symbol(self):
+        """
+        :return: случайно сгенерированная строка
+        """
         return ''.join(choice(ascii_uppercase) for i in range(self.count))
 
 
